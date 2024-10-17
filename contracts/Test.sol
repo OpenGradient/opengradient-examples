bda016a4dfc6588b1dc9e4eea0764075176f2beb
kukac <adambalogh@users.noreply.github.com>
2024-10-17 16:46:04 +0200

    Update Test.sol

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "import "opengradient-neuroml/src/OGInference.sol";
+import "opengradient-neuroml/src/OGInference.sol";
 
 contract Test {
 
